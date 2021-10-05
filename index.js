c09c0c5be65e68e4821460438c2ca704af87bb05
Maria <mariagiraldo4@gmail.com>
2021-10-05 19:20:06 +0000

    index added

---
+++ index.js
@@ -0,0 +1,29 @@
+//const express = require("express");
+import express from "express";
+import { icecreamCtrl } from "./controllers/index.js";
+
+import { heladosRoutes } from "./routes/index.js";
+
+console.log("ROUTES ", heladosRoutes);
+
+// Create app
+const app = express();
+
+// use json
+app.use(express.json());
+
+// Routes icecream
+app.get(heladosRoutes.GET, icecreamCtrl.getAll);
+app.get(heladosRoutes.GET_ONE, icecreamCtrl.getOne);
+app.post(heladosRoutes.CREATE, icecreamCtrl.create);
+app.put(heladosRoutes.UPDATE, icecreamCtrl.update);
+app.delete(heladosRoutes.DELETE, icecreamCtrl.deleteOne);
+
+// isers routes
+
+// ...
+
+// Launch server
+app.listen(3004, () => {
+  console.log("Initialized server...");
+});
